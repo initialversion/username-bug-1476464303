c782e97e983af6744d53b9ede92cf813431ed045
Raghu Betina <raghu@firstdraft.com>
2016-10-14 17:02:19 +0000

    Generate merchants resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :merchants
+  # Routes for the Merchant resource:
+  # READ
+  get "/merchants", :controller => "merchants", :action => "index"
+  get "/merchants/:id", :controller => "merchants", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20161014170219_add_password_to_merchants.rb
@@ -0,0 +1,5 @@
+class AddPasswordToMerchants < ActiveRecord::Migration[5.0]
+  def change
+    add_column :merchants, :password, :string
+  end
+end
